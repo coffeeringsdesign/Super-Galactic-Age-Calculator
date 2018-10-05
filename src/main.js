8adc1f739582d3a9c1e0af18eb1a2b0afc4aaa92
Kendra <kendra@coffeeringsdesign.com>
2018-10-05 15:32:59 -0700

    get all calculations to be correct and appear on the UI

--- src/main.js
+++ src/main.js
@@ -18,11 +18,24 @@ $(document).ready(function(){
     $("#answer4").text(visitor1.marsAge);
     $("#answer5").text(visitor1.jupiterAge);
 
+    if (visitor1.earthAge <= lifeExpectancyInput) {
+      $(".yearsLeftonPlanets").show();
+    } else if (visitor1.earthAge > lifeExpectancyInput) {
+      $(".yearsPastExpectation").show();
+    }
+
     $("#answer6").text(visitor1.earthLifeRemaining);
     $("#answer7").text(visitor1.mercuryLifeRemaining);
     $("#answer8").text(visitor1.venusLifeRemaining);
     $("#answer9").text(visitor1.marsLifeRemaining);
     $("#answer10").text(visitor1.jupiterLifeRemaining);
+    $("#answer11").text(Math.abs(visitor1.earthLifeRemaining));
+    $("#answer12").text(Math.abs(visitor1.mercuryLifeRemaining));
+    $("#answer13").text(Math.abs(visitor1.venusLifeRemaining));
+    $("#answer14").text(Math.abs(visitor1.marsLifeRemaining));
+    $("#answer15").text(Math.abs(visitor1.jupiterLifeRemaining));
+
+
 
   })
 });
