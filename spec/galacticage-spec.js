9cc059db3d7cbb6afb22be8e49f201a8c106f651
Kendra <kendra@coffeeringsdesign.com>
2018-10-05 14:42:13 -0700

    get all initial planet age tests to return failed, and then correct them to return successful

--- spec/galacticage-spec.js
+++ spec/galacticage-spec.js
@@ -1,8 +1,28 @@
 import Visitor from './../src/userObject.js';
 
 describe ('AgeCalculator', function() {
-  it('should test if a visitor enters their date of birth it should return their age in earth years', function() {
+  it('should test if a visitor enters their age it should return their age in earth years', function() {
     let visitor1 = new Visitor(24);
     expect(visitor1.earthAge).toEqual(24);
+  });
+
+  it('should test if a visitor enters their age it should return their age in mercury years', function() {
+    let visitor1 = new Visitor(24);
+    expect(visitor1.mercuryAge).toEqual(visitor1.earthAge/.24);
+  });
+
+  it('should test if a visitor enters their age it should return their age in venus years', function() {
+    let visitor1 = new Visitor(24);
+    expect(visitor1.venusAge).toEqual(visitor1.earthAge/.62);
+  });
+
+  it('should test if a visitor enters their age it should return their age in mars years', function() {
+    let visitor1 = new Visitor(24);
+    expect(visitor1.marsAge).toEqual(visitor1.earthAge/1.88);
+  });
+
+  it('should test if a visitor enters their age it should return their age in jupiter years', function() {
+    let visitor1 = new Visitor(24);
+    expect(visitor1.jupiterAge).toEqual(visitor1.earthAge/11.86);
   });
 });
--- src/userObject.js
+++ src/userObject.js
@@ -2,8 +2,9 @@ export default class Visitor {
   constructor(ageInput) {
     // add back in lifeExpectancyInput
     this.earthAge = ageInput;
-    // this.mercuryAge = mercuryAge;
-    // this.venusAge = venusAge;
-    // this.jupiterAge = jupiterAge;
+    this.mercuryAge = parseFloat(ageInput / .24);
+    this.venusAge = parseFloat(ageInput / .62);
+    this.marsAge = parseFloat(ageInput / 1.88);
+    this.jupiterAge = parseFloat(ageInput / 11.86);
   };
 }
